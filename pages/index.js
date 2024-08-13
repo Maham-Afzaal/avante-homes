70b7c958a1af337383c002266a3017ad79ebb94b
saroshfarooq <sarosh.faroq@gmail.com>
2024-08-13 11:58:33 +0500

    update home page background

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import HomePage from "@/components/Home";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
@@ -11,7 +12,19 @@ export default function Home() {
         <meta property="og:description" content="Home - Avante Homes" />
         <meta property="og:image" content="https://example.com/image.jpg" />
       </Head>
-      <HomePage />
+      <Box
+        sx={{
+          height: { xs: "1026px", md: "777px" },
+          width: "100%",
+          background: "linear-gradient(0deg, #FFFFFF 0%, #EFF2FF 100%);",
+          position: "absolute",
+          top: "0px",
+          zIndex: 0,
+        }}
+      ></Box>
+      <Box sx={{ zIndex: 1000, position: "relative" }}>
+        <HomePage />
+      </Box>
     </>
   );
 }
